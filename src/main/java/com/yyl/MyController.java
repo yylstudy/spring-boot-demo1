409aa6defb137ab4c7770375f53bb3880212b08c
wb-yyl715977 <wb-yyl715977@alibaba-inc.com>
2020-04-23 09:49:34 +0800

    ssss

--- src/main/java/com/yyl/MyController.java
+++ src/main/java/com/yyl/MyController.java
@@ -17,7 +17,7 @@ public class MyController {
 
     @RequestMapping("/test1")
     public String test1(){
-        return "hello world ssss"+env;
+        return "hello world rrrrr"+env;
     }
     @RequestMapping("/test2")
     public String test2(){
